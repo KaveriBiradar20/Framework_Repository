8c8ed0df594f2da372d812713ce7d2b4b1db3623
KaveriBiradar <kaveribiradar178@gmail.com>
2024-01-13 12:04:38 +0530

    sampletest comiit is done

---
+++ src/main/java/SampleTestforjenkins.java
@@ -0,0 +1,10 @@
+
+public class SampleTestforjenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is for GitIntegration with Jenkins");
+
+	}
+
+}
