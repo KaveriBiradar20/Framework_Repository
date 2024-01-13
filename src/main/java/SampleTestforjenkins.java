8e55c566fe9d8cd6b62c533f7c61f0926509648e
KaveriBiradar <kaveribiradar178@gmail.com>
2024-01-13 12:16:03 +0530

    added one more line

--- src/main/java/SampleTestforjenkins.java
+++ src/main/java/SampleTestforjenkins.java
@@ -4,6 +4,7 @@ public class SampleTestforjenkins {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is for GitIntegration with Jenkins");
+		System.out.println("This is for GitIntegration with Jenkins");
 
 	}
 
